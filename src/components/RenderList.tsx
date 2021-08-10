98bd542ad4c5981416fdfd76889f2906d83478ec
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2021-08-10 19:24:24 +0500

    Adding loading text

--- src/components/RenderList.tsx
+++ src/components/RenderList.tsx
@@ -5,43 +5,47 @@ import { Item } from "./Item";
 export const RenderList = ({ items = [] }: { items: ItemProps[] }) => {
   return (
     <>
-      <Box
-        overflowY="auto"
-        minW={{ base: "95vw", md: "80vw" }}
-        maxH="80vh"
-        mt="10"
-        p="10"
-        shadow="lg"
-        css={{
-          "&::-webkit-scrollbar": {
-            width: "7px",
-          },
-          "&::-webkit-scrollbar-track": {
-            width: "10px",
-          },
-          "&::-webkit-scrollbar-thumb": {
-            background: "lightgray",
-            borderRadius: "24px",
-          },
-        }}
-      >
-        <VStack
-          divider={
-            <StackDivider
-              borderColor="black.500"
-              shadow="dark-lg"
-              width="30%"
-              alignSelf="center"
-            />
-          }
-          spacing={4}
-          align="center"
+      {items.length ? (
+        <Box
+          overflowY="auto"
+          minW={{ base: "95vw", md: "80vw" }}
+          maxH="80vh"
+          mt="10"
+          p="10"
+          shadow="lg"
+          css={{
+            "&::-webkit-scrollbar": {
+              width: "7px",
+            },
+            "&::-webkit-scrollbar-track": {
+              width: "10px",
+            },
+            "&::-webkit-scrollbar-thumb": {
+              background: "lightgray",
+              borderRadius: "24px",
+            },
+          }}
         >
-          {items?.length
-            ? items.map((item) => <Item item={item} key={item?.id} />)
-            : "No items"}
-        </VStack>
-      </Box>
+          <VStack
+            divider={
+              <StackDivider
+                borderColor="black.500"
+                shadow="dark-lg"
+                width="30%"
+                alignSelf="center"
+              />
+            }
+            spacing={4}
+            align="center"
+          >
+            {items.map((item) => (
+              <Item item={item} key={item?.id} />
+            ))}
+          </VStack>
+        </Box>
+      ) : (
+        <Box mt="20">Loading Items .......</Box>
+      )}
     </>
   );
 };
