983f52924466bf6e8049e077c65750887f27cc96
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2021-08-10 23:01:28 +0500

    update image width

--- src/components/ItemImg.tsx
+++ src/components/ItemImg.tsx
@@ -3,8 +3,8 @@ import { Box, Image } from "@chakra-ui/react";
 export const ItemImg = ({ img }: { img: string }) => {
   return (
     <>
-      <Box width={{ base: "25%", md: "20%" }}>
-        <Image src={img} h="160" w="250" objectFit="cover" alt="Model Image" />
+      <Box width={{ base: "25%", md: "25%" }}>
+        <Image src={img} h="160" w="300" objectFit="cover" alt="Model Image" />
       </Box>
     </>
   );
