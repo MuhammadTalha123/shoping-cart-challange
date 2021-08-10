dc9b0e7ee15e2772e6c9baf139eda6df2c6fb4d6
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2021-08-10 19:18:51 +0500

    Adding filter by color functionality

--- src/components/ColorFilter.tsx
+++ src/components/ColorFilter.tsx
@@ -1,12 +1,22 @@
+import { ChangeEventHandler } from "react";
 import { Box, Select } from "@chakra-ui/react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { InitialStateProps } from "../interfaces/redux";
+import { setFilteredItems } from "../redux/actions";
 
 export const ColorFilter = () => {
-  const { filterColors } = useSelector((state: InitialStateProps) => state);
+  const { filterColors, items } = useSelector(
+    (state: InitialStateProps) => state
+  );
+  const dispatch = useDispatch();
+  const onChangeHandler: ChangeEventHandler<HTMLSelectElement> = (event) => {
+    const selectedValue = event.target.value;
+    const filteredItems = items.filter((item) => item.colour === selectedValue);
+    dispatch(setFilteredItems(filteredItems));
+  };
   return (
     <Box mt="10" w="60">
-      <Select placeholder="Available Colors">
+      <Select placeholder="All Available Colors" onChange={onChangeHandler}>
         {filterColors.map((color) => (
           <option key={color} value={color}>
             {color}
--- src/redux/actions.ts
+++ src/redux/actions.ts
@@ -9,3 +9,8 @@ export const setFilterColors = (payload: string[]) => ({
   type: "SET_FILTERE_COLORS",
   payload,
 });
+
+export const setFilteredItems = (payload: ItemProps[]) => ({
+  type: "SET_FILTERED_ITEMS",
+  payload,
+});
