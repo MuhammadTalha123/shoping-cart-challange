cf4f4018003d0fd24bb899047de5696282399a8e
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2021-08-10 20:08:28 +0500

    Removing extra calls to API

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Flex } from "@chakra-ui/react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getItems } from "./api/GetItem";
 import { ColorFilter } from "./components/ColorFilter";
@@ -10,28 +10,33 @@ import { setFilterColors, setItems } from "./redux/actions";
 
 const App = () => {
   const dispatch = useDispatch();
+  const [loading, setLoading] = useState(false);
   const { items, filteredItems } = useSelector(
     (state: InitialStateProps) => state
   );
 
   useEffect(() => {
-    getItems().then((data: any) => {
-      console.log(data);
-      let filterColors: string[] = [];
-      const updatedData = data?.map((item: ItemProps) => {
-        const exists = filterColors.find((color) => color === item.colour);
-        if (!exists) {
-          filterColors.push(item.colour);
-        }
-        return {
-          ...item,
-          selected: 0,
-        };
+    if (!loading) {
+      setLoading(true);
+      getItems().then((data: any) => {
+        console.log(data);
+        let filterColors: string[] = [];
+        const updatedData = data?.map((item: ItemProps) => {
+          const exists = filterColors.find((color) => color === item.colour);
+          if (!exists) {
+            filterColors.push(item.colour);
+          }
+          return {
+            ...item,
+            selected: 0,
+          };
+        });
+        dispatch(setItems(updatedData));
+        dispatch(setFilterColors(filterColors));
       });
-      dispatch(setItems(updatedData));
-      dispatch(setFilterColors(filterColors));
-    });
+    }
   });
+
   return (
     <Flex alignItems="center" maxH="85vh" flexDir="column">
       <ColorFilter />
